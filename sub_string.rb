7c26638bdb368b57478fd115da6d006610151439
taha <297468732+name-abacus-mud@users.noreply.github.com>
2026-08-28 02:32:49 +0530

    added linting support through gemfiles and rubocop

--- sub_string.rb
+++ sub_string.rb
@@ -1,12 +1,11 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# frozen_string_literal: true
 
 def substrings(string, dictionary)
   result = Hash.new(0)
   string.downcase!
   dictionary.each do |word|
     count = string.scan(/(?=#{word})/).count
-    result[word] = count if count > 0
+    result[word] = count if count.positive?
   end
   result
 end
-
